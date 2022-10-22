9b412a3fe9ad154e42ece959e1e1a0f77b1aacff
DanielCabralBarbosa <77117506+DanielCabralBarbosa@users.noreply.github.com>
2022-10-22 00:42:02 -0300

    Mutex
    Implementação do Mutex sincronizando as threads

--- RPSDanielThiago/src/br/com/danielcabral/git/MainClass.java
+++ RPSDanielThiago/src/br/com/danielcabral/git/MainClass.java
@@ -4,6 +4,9 @@ public class MainClass {
 	
 	public static void main(String[]args) {
 		Player player1 = new Player("Carlos");
+		Player player2 = new Player("Pedro");
+		
 		player1.start();
+		player2.start();
 	}
 }
--- RPSDanielThiago/src/br/com/danielcabral/git/Player.java
+++ RPSDanielThiago/src/br/com/danielcabral/git/Player.java
@@ -1,12 +1,14 @@
 package br.com.danielcabral.git;
 
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Player extends Thread {
 		
 	String name;
 	String[] plays = {"Pedra", "Papel", "Tesoura"};
 	Random random;
+	static Semaphore semaphore = new Semaphore(1);
 	
 	public Player(String _name)	{
 		this.name = _name;
@@ -19,7 +21,39 @@ public class Player extends Thread {
 	}
 		
 	public void run() {
-		System.out.println("Jogada de " + this.name + ": " + this.play());
+		
+		try {
+			
+			System.out.println(this.name + " : Se preparando " 
+			+ semaphore.availablePermits());
+			
+			semaphore.acquire();
+			
+			System.out.println(" Vez do : " + this.name);
+			
+			try {
+				
+				System.out.println("Jogada de " + this.name + ": " + this.play() 
+				+ semaphore.availablePermits());
+				
+				//sleep
+				Thread.sleep(1000);		
+				
+				
+			} finally {
+				
+				System.out.println(this.name + "passa a vez");
+				semaphore.release();
+				
+				
+				System.out.println(" Próximo " 
+						+ semaphore.availablePermits());
+				
+			}
+			
+		}catch (InterruptedException e) {e.printStackTrace();}
+			
+			
+		}
+		
 	}
-	
-}
