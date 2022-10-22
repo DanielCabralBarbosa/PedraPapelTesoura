b2b33549bb7b2b552f15cda3a258b7a7ebd24e65
TgCruzz <87619682+TgCruzz@users.noreply.github.com>
2022-10-22 17:06:53 -0300

    Teste de vitórias (falho)

--- RPSDanielThiago/src/br/com/danielcabral/git/MainClass.java
+++ RPSDanielThiago/src/br/com/danielcabral/git/MainClass.java
@@ -2,11 +2,35 @@ package br.com.danielcabral.git;
 
 public class MainClass {
 	
+	
 	public static void main(String[]args) {
 		Player player1 = new Player("Carlos");
 		Player player2 = new Player("Pedro");
-		
 		player1.start();
 		player2.start();
+		
+		if(player1.currentPlay == player2.currentPlay) {
+			System.out.println("Empate!");
+		}
+		else if(player1.currentPlay == player1.plays[0]) {
+			if(player2.currentPlay == player2.plays[2]) 
+				System.out.println("Vitória de " + player1.name);
+			else 
+				System.out.println("Vitória de " + player2.name);			
+		}
+		else if(player1.currentPlay == player1.plays[1]) {
+			if(player2.currentPlay == player2.plays[0]) 
+				System.out.println("Vitória de " + player1.name);
+			else 
+				System.out.println("Vitória de " + player2.name);			
+		}
+		else if(player1.currentPlay == player1.plays[2]) {
+			if(player2.currentPlay == player2.plays[1]) 
+				System.out.println("Vitória de " + player1.name);
+			else
+				System.out.println("Vitória de " + player2.name);			
+		}
+		
 	}
+	
 }
--- RPSDanielThiago/src/br/com/danielcabral/git/Player.java
+++ RPSDanielThiago/src/br/com/danielcabral/git/Player.java
@@ -5,36 +5,35 @@ import java.util.concurrent.Semaphore;
 
 public class Player extends Thread {
 		
-	String name;
+	String name, currentPlay;
 	String[] plays = {"Pedra", "Papel", "Tesoura"};
 	Random random;
 	static Semaphore semaphore = new Semaphore(1);
 	
 	public Player(String _name)	{
 		this.name = _name;
-		random = new Random(System.currentTimeMillis());
+		this.random = new Random();
 	}
 	
 	public String play() {
-		int x = random.nextInt(3);	
-		return plays[x];
+		int x = this.random.nextInt(3);	
+		this.currentPlay = plays[x];
+		return currentPlay;
 	}
 		
 	public void run() {
 		
 		try {
 			
-			System.out.println(this.name + " : Se preparando " 
-			+ semaphore.availablePermits());
+			System.out.println(this.name + ": Se preparando ");
 			
 			semaphore.acquire();
 			
-			System.out.println(" Vez do : " + this.name);
+			System.out.println(" Vez de: " + this.name);
 			
 			try {
 				
-				System.out.println("Jogada de " + this.name + ": " + this.play() 
-				+ semaphore.availablePermits());
+				System.out.println("Jogada de " + this.name + ": " + this.play());
 				
 				//sleep
 				Thread.sleep(1000);		
@@ -42,12 +41,12 @@ public class Player extends Thread {
 				
 			} finally {
 				
-				System.out.println(this.name + "passa a vez");
+				System.out.println(this.name + " passa a vez");
 				semaphore.release();
 				
 				
-				System.out.println(" Próximo " 
-						+ semaphore.availablePermits());
+				System.out.println(" Proximo ");
+				System.out.println(" ");
 				
 			}
 			
